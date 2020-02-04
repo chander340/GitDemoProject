21cb057bd26933b95cbede10ab3c07608c808ae2
Nitin Jaswal <nitinj@whizkraft.net>
2020-02-04 13:30:12 +0530

    Add Label and images in Storyboard

--- GitDemoProject/ViewController.swift
+++ GitDemoProject/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  GitDemoProject
-//
-//  Created by priyanka on 04/02/20.
-//  Copyright © 2020 imminentSoftwares. All rights reserved.
-//
 
 import UIKit
 
@@ -12,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
